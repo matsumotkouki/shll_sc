b394b64c9a395697d67aaa4d279118e768a2d09c
matsumotokouki <18matsumoto@nao.net.it-chiba.ac.jp>
2018-06-01 17:53:07 +0900

    all student pass issue

--- cg/pass_issue.sh
+++ cg/pass_issue.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 student_id=13
-student_number=hoge
+student_number=1632001
 password=hoge
 lain=1
 fast=0
-loopmax=32
+loopmax=1632170
 for ((i=0;i<$loopmax;i++))
 do
 	if [ $fast -eq 0  ]; then
-		student_name=$(ls | grep js | sed -n "$lain"p | tr -d .js)
+		let student_name++
 		password=$(openssl rand -base64 12 | fold -w 6 | head -1)
 		echo "($student_id, '${student_name}', '${password}')," > password_database.txt
 		let fast++
 		let student_id++
 		let lain++
 	else 
-		student_name=$(ls | grep js | sed -n "$lain"p | tr -d .js)
+		let student_name++
 		password=$(openssl rand -base64 12 | fold -w 6 | head -1)
 		echo "($student_id, '${student_name}', '${password}')," >> password_database.txt
 		let student_id++
