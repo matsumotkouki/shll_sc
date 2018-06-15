91e11b28f69e9b4c79c2b45537f941b3a7736913
matsumotokouki <18matsumoto@nao.net.it-chiba.ac.jp>
2018-06-15 17:13:21 +0900

    js_conbert_dirにobjファイルを入れればコンバートできるように設定

--- cg/js_convert.sh
+++ cg/js_convert.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-obj=1632002
-js=1632002
-max=1632162
-pas=./Documents/hand_obj/
+pas=./Documents/js_convert_dir
+line=1
+max=$(ls | grep obj | wc -l)
 
-while [ $obj -le $max ]
+while [ $line -le $max ]
 do
-	python convert_obj_three.py -i $obj.obj -o $js.js
-	let obj++
-	let js++S
+	cd $pas
+	obj=$(ls | grep obj | sed -n "$line"p)
+	js=$(ls | grep fbx | sed -n "$line"p | tr -d .fbx)
+	python convert_obj_three.py -i $obj -o $js.js
+	let line++
 done
