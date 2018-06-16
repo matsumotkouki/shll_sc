05d9e18ebfcee5a96da23741b26333ef4ff350d7
matsumotokouki <18matsumoto@nao.net.it-chiba.ac.jp>
2018-06-16 17:25:16 +0900

    ファイル名修正

--- cg/fbx_obj_convert_add.sh
+++ cg/fbx_obj_convert_add.sh
@@ -10,6 +10,6 @@ do
 	filename=$(ls | grep fbx | sed -n "$line"p | sed -e "s/.fbx//")
     echo "file -f -options \"fbx\"  -ignoreVersion  -typ \"FBX\" -o \"C:/Users/share pc/Documents/cg/2sec/model/hand2/${filename}.fbx\";addRecentFile(\"C:/Users/share pc/Documents/cg/2sec/model/hand2/${filename}.fbx\", \"FBX\");
 file -force -options \"groups=1;ptgroups=1;materials=1;smoothing=1;normals=1\" -type \"OBJexport\" -pr -ea \"C:/Users/share pc/Documents/cg/2sec/model/hand2/OBJ/${filename}.obj\";
-NewScene; " >> ${mel_pass}fbx_obj_conbert.mel
+NewScene; " >> ${mel_pass}fbx_obj_convert.mel
 	let line++
 done
