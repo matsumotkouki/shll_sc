5b3937e9744a5bf0dd14847096ddf936b010c3e6
matsumotokouki <18matsumoto@nao.net.it-chiba.ac.jp>
2018-06-17 11:58:30 +0900

    使いやすいように修正

--- cg/js_movement.sh
+++ cg/js_movement.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
-start=1632002
-end=1632162
+#!/bin/bash
+pass=/Users/matsumoto/Documents/CG_convert_dir/js_convert_dir
+line=1
+max=$(ls "$pass" | grep obj | wc -l)
 
-while [ $start -le $end ]
+while [ $line -le $max ]
 do
-	cp $start.js js/$start.js
-	let start++
+	cd $pass
+	filename=$(ls | grep js | sed -n "$line"p | sed -e "s/.js//")
+	cp $filename.js js/$filename.js
+	let line++
 done
